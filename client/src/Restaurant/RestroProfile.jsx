a16d08672f48306f53e7429b6579c83419f11c3c
vsbadona <vishalsainisilu@gmail.com>
2024-03-29 02:41:12 +0530

    removed warning

--- client/src/Restaurant/RestroProfile.jsx
+++ client/src/Restaurant/RestroProfile.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { checkLogin } from '../Redux/foodSlice'
-import { addCategory, findCategories, removeCategory, updatesellerProfile } from '../Redux/CRUDUser'
+import { addCategory, findCategories, removeCategory } from '../Redux/CRUDUser'
 import axios from 'axios'
 
 const RestroProfile = () => {
